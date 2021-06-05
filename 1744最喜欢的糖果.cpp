abed88fe3490196fa6e35fb65c94ee8010f12691
Levine <levinestark@163.com>
2021-06-05 22:12:01 +0800

    添加了1744最喜欢的糖果

---
+++ 1744最喜欢的糖果.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<bool> canEat(vector<int> &candiesCount, vector<vector<int>> &queries)
+    {
+        // 初始化需要带上长度
+        vector<long long> pre_sum(candiesCount.size());
+        pre_sum[0] = candiesCount[0];
+        for (int i = 1; i < candiesCount.size(); i++)
+        {
+            pre_sum[i] = pre_sum[i - 1] + candiesCount[i];
+        }
+        vector<bool> answer;
+        for (int k = 0; k < queries.size(); k++)
+        {
+            long long min = queries[k][1] + 1;
+            long long max = min * queries[k][2];
+            long long lowersum = (queries[k][0] == 0 ? 1 : pre_sum[queries[k][0] - 1] + 1);
+            answer.push_back(!(lowersum > max || pre_sum[queries[k][0]] < min));
+        }
+        return answer;
+    }
+};

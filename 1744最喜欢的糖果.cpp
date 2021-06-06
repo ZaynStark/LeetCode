1cdbd0f779b32617f5cc203715e12d4b308f178d
Levine <levinestark@163.com>
2021-06-06 13:45:16 +0800

    改变了遍历vector的方法

--- 1744最喜欢的糖果.cpp
+++ 1744最喜欢的糖果.cpp
@@ -14,6 +14,14 @@ public:
             pre_sum[i] = pre_sum[i - 1] + candiesCount[i];
         }
         vector<bool> answer;
+        // 记录一下const auto&x:range
+        for (const auto &q : queries)
+        {
+            long long min = q[1] + 1;
+            long long max = min * q[2];
+            long long lowersum = (q[0] == 0 ? 1 : pre_sum[q[0] - 1] + 1);
+            answer.push_back(!(lowersum > max || pre_sum[q[0]] < min));
+        }
         for (int k = 0; k < queries.size(); k++)
         {
             long long min = queries[k][1] + 1;

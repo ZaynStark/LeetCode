95c1c4d93852e285ba03851afabf344dbacce97f
Levine <levinestark@163.com>
2021-06-09 12:53:14 +0800

    添加了1049最后一块石头的重量2

---
+++ 1049最后一块石头的重量2.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <numeric>
+using namespace std;
+class Solution
+{
+public:
+    int lastStoneWeightII(vector<int> &stones)
+    {
+        int sum = accumulate(stones.begin(), stones.end(), 0);
+        int m = sum / 2;
+        vector<int> dp(m + 1);
+        dp[0] = true;
+        for (int weight : stones)
+        {
+            for (int j = m; j >= weight; --j)
+            {
+                dp[j] = dp[j] || dp[j - weight];
+            }
+        }
+        for (int j = m;; --j)
+        {
+            if (dp[j])
+            {
+                return sum - 2 * j;
+            }
+        }
+    }
+};
